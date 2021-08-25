8c144baba833254fdbb22a9aa6ce75ebeef4f8f0
Jose S. Daniel <djose1164@gmail.com>
2021-08-24 22:37:31 -0400

    Fix and add new type.

--- include/utils.h
+++ include/utils.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SET_RED_FOREGROUND printf("\033[0;31m")
-#define SET_YELLOW_FOREGROUND printf("\033[0;33m")
+#define SET_RED_FG printf("\033[0;31m")
+#define SET_YELLOW_FG printf("\033[0;33m")
+#define GREEN_FG "\x1B[32m"
+#define YELLOW_FG "\033[0;33m"
+#define RED_FG "\033[0;31m"
+#define RESET_FG "\033[0m"
 #define RESET_COLOR printf("\033[0m")
 
 #define _ERR "Error code:"
@@ -22,6 +26,23 @@
 #else
 #define sleep(time) Sleep((time)*1000))
 #endif // __linux__
+
+enum message_type
+{
+    NORMAL,
+    CONNECT,
+    DISCONNECT,
+    SUCCESS,
+    ERROR
+};
+
+struct message
+{
+    enum message_type type;
+    char username[50];
+    char data[1024];
+};
+
 
 static inline void utils_clear_terminal(void)   
 {

997a5149185100ebce9d50952af7da643aab5459
Jose S. Daniel <djose1164@gmail.com>
2021-08-24 22:38:50 -0400

    Fix and defines funcs.

--- src/client.c
+++ src/client.c
@@ -2,7 +2,7 @@
 
 void client_init(struct client *client, const char *name, const char *port)
 {
-    SET_YELLOW_FOREGROUND;
+    SET_YELLOW_FG;
 
     printf("Configuring client...\n");
     struct addrinfo hints;
@@ -36,18 +36,21 @@ void client_init(struct client *client, const char *name, const char *port)
 
 void client_start(struct client *client)
 {
-    SET_YELLOW_FOREGROUND;
+    SET_YELLOW_FG;
     puts("Starting connection...\n");
     if (connect(client->peer_socket, client->peer_address->ai_addr,
                 client->peer_address->ai_addrlen) == -1)
         die("connect() failed", GET_SOCKET_ERRNO());
     freeaddrinfo(client->peer_address);
+    
     puts("\t\t\a-> Connected <-\n");
-
-    SET_RED_FOREGROUND;
+    client_connected(client->peer_socket, client->name);
+    
+    SET_RED_FG;
     printf("To send a message press enter.\n"
-         "-> ");
+           "-> ");
     fflush(stdout);
+
     while (true)
     {
         fd_set reads;
@@ -64,44 +67,89 @@ void client_start(struct client *client)
         if (select(client->peer_socket + 1, &reads, 0, 0, &timeout) < 0)
             die("select() failed", GET_SOCKET_ERRNO());
 
+        // Handle server response.
         if (FD_ISSET(client->peer_socket, &reads))
-        {
-            char read[4096];
-            int bytes_received = recv(client->peer_socket, read, sizeof(read), 0);
-            if (bytes_received < 1)
-            {
-                puts("Connection closed by server.\n");
-                break;
-            }
-
-            printf("Received: %s\n",read);
-        }
-
+            client_recv_from_serv(client);
         // ----------------
         // Sending message.
         // ----------------
-#ifdef __linux__
-        if (FD_ISSET(fileno(stdin), &reads))
-        {
-#else
-        if (_kbhit())
-        {
-#endif //__linux__
-            char read[4096];
-            if (!fgets(read, sizeof(read), stdin))
-                break;
-            read[strcspn(read, "\n")] = 0;
-            socket_send(client->peer_socket, read, strlen(read), 0);
-            puts("Sent!");
-            printf("%s", "-> ");
-            fflush(stdout);
-        }
+        client_handle_input(client, &reads);
     }
+}
+
+void client_recv_from_serv(struct client *client)
+{
+    struct message read;
+    memset(&read, 0, sizeof(read));
+
+    int bytes_received = recv(client->peer_socket, &read, sizeof(read), 0);
+    if (bytes_received < 1)
+    {
+        puts("\a\rConnection closed by server.\n");
+        client_end(client->peer_socket);
+    }
+
+    switch (read.type)
+    {
+    case CONNECT:
+        printf(GREEN_FG "\r%s " RED_FG "has connected.\n", read.username);
+        break;
+
+    case DISCONNECT:
+        printf(GREEN_FG "\r%s " RED_FG "has disconnected.\n", read.username);
+        break;
+        
+    case NORMAL:
+        printf(GREEN_FG "\r\a%s" RED_FG ": %s\n", read.username, read.data);
+        break;
+
+    default:
+        RESET_COLOR;
+        fprintf(stderr, "\rUknown type message.\n");
+        break;
+    }
+    printf("-> ");
+}
+
+void client_handle_input(struct client *client, fd_set *reads)
+{
+#ifdef __linux__
+    if (FD_ISSET(fileno(stdin), reads))
+    {
+#else
+    if (_kbhit())
+    {
+#endif //__linux__
+        char read[1024] = "";
+
+        struct message msg;
+        memset(&msg, 0, sizeof(msg));
+        msg.type = NORMAL;
+
+        strncpy(msg.username, client->name, sizeof(client->name));
+        if (!fgets(read, sizeof(read), stdin))
+            return;
+        read[strcspn(read, "\n")] = 0;
+
+        strncpy(msg.data, read, sizeof(read));
+
+        socket_send(client->peer_socket, &msg, sizeof(msg), 0);
+        printf("%s", "-> ");
+    }
+    fflush(stdout);
+}
+
+void client_connected(SOCKET _socket, const char *username)
+{
+    struct message msg = {0};
+    strncpy(msg.username, username, sizeof(msg.username));
+    msg.type = CONNECT;
+    socket_send(_socket, &msg, sizeof(msg), 0);
 }
 
 void client_end(SOCKET _socket)
 {
-    SET_YELLOW_FOREGROUND;
+    SET_YELLOW_FG;
     puts("Closing program...\n"
          "---- Closing socket...\n");
     CLOSE_SOCKET(_socket);
@@ -110,4 +158,5 @@ void client_end(SOCKET _socket)
     WSACleanup();
 #endif //__WIN32
     puts("\aBye!\n");
+    exit(0);
 }
